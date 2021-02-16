d09c9b988d65ca0bdb2579d7199ad4799d8c71e3
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2021-02-16 15:25:25 +0530

    temp: removed firebase

--- pollywallet/lib/screens/settings_screen/export_mnemonic.dart
+++ pollywallet/lib/screens/settings_screen/export_mnemonic.dart
@@ -4,16 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/io_client.dart';
 import 'package:pollywallet/theme_data.dart';
 import 'package:share/share.dart';
-import 'package:googleapis/drive/v3.dart' as ga;
 import 'package:http/http.dart' as http;
 
-import 'package:file_picker/file_picker.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
 
@@ -24,86 +19,86 @@ class ExportMnemonic extends StatefulWidget {
 
 class _ExportMnemonicState extends State<ExportMnemonic> {
   String mnemonic;
-  final GoogleSignIn googleSignIn =
-      GoogleSignIn(scopes: ['https://www.googleapis.com/auth/drive.appdata']);
-  GoogleSignInAccount googleSignInAccount;
-  ga.FileList list;
-  final storage = new FlutterSecureStorage();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  var signedIn = false;
+  // final GoogleSignIn googleSignIn =
+  //     GoogleSignIn(scopes: ['https://www.googleapis.com/auth/drive.appdata']);
+  // GoogleSignInAccount googleSignInAccount;
+  // ga.FileList list;
+  // final storage = new FlutterSecureStorage();
+  // final FirebaseAuth _auth = FirebaseAuth.instance;
+  // var signedIn = false;
 
   @override
   void initState() {
     super.initState();
   }
 
-  Future<void> _loginWithGoogle() async {
-    signedIn = await storage.read(key: "signedIn") == "true" ? true : false;
-    googleSignIn.onCurrentUserChanged
-        .listen((GoogleSignInAccount googleSignInAccount) async {
-      if (googleSignInAccount != null) {
-        _afterGoogleLogin(googleSignInAccount);
-      }
-    });
-    if (signedIn) {
-      try {
-        googleSignIn.signInSilently().whenComplete(() => () {});
-      } catch (e) {
-        storage.write(key: "signedIn", value: "false").then((value) {
-          setState(() {
-            signedIn = false;
-          });
-        });
-      }
-    } else {
-      final GoogleSignInAccount googleSignInAccount =
-          await googleSignIn.signIn();
-      _afterGoogleLogin(googleSignInAccount);
-    }
-  }
+  // Future<void> _loginWithGoogle() async {
+  //   signedIn = await storage.read(key: "signedIn") == "true" ? true : false;
+  //   googleSignIn.onCurrentUserChanged
+  //       .listen((GoogleSignInAccount googleSignInAccount) async {
+  //     if (googleSignInAccount != null) {
+  //       _afterGoogleLogin(googleSignInAccount);
+  //     }
+  //   });
+  //   if (signedIn) {
+  //     try {
+  //       googleSignIn.signInSilently().whenComplete(() => () {});
+  //     } catch (e) {
+  //       storage.write(key: "signedIn", value: "false").then((value) {
+  //         setState(() {
+  //           signedIn = false;
+  //         });
+  //       });
+  //     }
+  //   } else {
+  //     final GoogleSignInAccount googleSignInAccount =
+  //         await googleSignIn.signIn();
+  //     _afterGoogleLogin(googleSignInAccount);
+  //   }
+  // }
 
-  Future<void> _afterGoogleLogin(GoogleSignInAccount gSA) async {
-    googleSignInAccount = gSA;
-    final GoogleSignInAuthentication googleSignInAuthentication =
-        await googleSignInAccount.authentication;
+  // Future<void> _afterGoogleLogin(GoogleSignInAccount gSA) async {
+  //   googleSignInAccount = gSA;
+  //   final GoogleSignInAuthentication googleSignInAuthentication =
+  //       await googleSignInAccount.authentication;
 
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-      accessToken: googleSignInAuthentication.accessToken,
-      idToken: googleSignInAuthentication.idToken,
-    );
+  //   final AuthCredential credential = GoogleAuthProvider.getCredential(
+  //     accessToken: googleSignInAuthentication.accessToken,
+  //     idToken: googleSignInAuthentication.idToken,
+  //   );
 
-    final AuthResult authResult = await _auth.signInWithCredential(credential);
-    final FirebaseUser user = authResult.user;
+  //   final AuthResult authResult = await _auth.signInWithCredential(credential);
+  //   final FirebaseUser user = authResult.user;
 
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
+  //   assert(!user.isAnonymous);
+  //   assert(await user.getIdToken() != null);
 
-    final FirebaseUser currentUser = await _auth.currentUser();
-    assert(user.uid == currentUser.uid);
+  //   final FirebaseUser currentUser = await _auth.currentUser();
+  //   assert(user.uid == currentUser.uid);
 
-    print('signInWithGoogle succeeded: $user');
+  //   print('signInWithGoogle succeeded: $user');
 
-    storage.write(key: "signedIn", value: "true").then((value) {
-      setState(() {
-        signedIn = true;
-      });
-    });
-  }
+  //   storage.write(key: "signedIn", value: "true").then((value) {
+  //     setState(() {
+  //       signedIn = true;
+  //     });
+  //   });
+  // }
 
-  _uploadFileToGoogleDrive() async {
-    final directory = await getApplicationDocumentsDirectory();
-    File file = File("${directory.path}/mnemonic.txt");
-    await file.writeAsString('$mnemonic');
-    var client = GoogleHttpClient(await googleSignInAccount.authHeaders);
-    var drive = ga.DriveApi(client);
-    ga.File fileToUpload = ga.File();
-    fileToUpload.name = path.basename(file.absolute.path);
-    var response = await drive.files.create(
-      fileToUpload,
-      uploadMedia: ga.Media(file.openRead(), file.lengthSync()),
-    );
-    print(response);
-  }
+  // _uploadFileToGoogleDrive() async {
+  //   final directory = await getApplicationDocumentsDirectory();
+  //   File file = File("${directory.path}/mnemonic.txt");
+  //   await file.writeAsString('$mnemonic');
+  //   var client = GoogleHttpClient(await googleSignInAccount.authHeaders);
+  //   var drive = ga.DriveApi(client);
+  //   ga.File fileToUpload = ga.File();
+  //   fileToUpload.name = path.basename(file.absolute.path);
+  //   var response = await drive.files.create(
+  //     fileToUpload,
+  //     uploadMedia: ga.Media(file.openRead(), file.lengthSync()),
+  //   );
+  //   print(response);
+  // }
 
   @override
   Widget build(BuildContext context) {

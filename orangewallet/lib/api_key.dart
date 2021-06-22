a582dd00df59966ef033f2218e1b51ab4144a82c
Juliana / @itsencrypted <49042388+itsencrypted@users.noreply.github.com>
2021-06-22 12:08:44 +0100

    changes updated SCripto

--- orangewallet/lib/api_key.dart
+++ orangewallet/lib/api_key.dart
@@ -1,2 +1,3 @@
-String CovalentKey = "ckey_780ed3c9aba3496e8e9948bada0";
-String EtherscanKey = "ZE2QGS32E2DTA2P37IXQG9Z5DT81QQV5C8";
+// String CovalentKey = "ckey_780ed3c9aba3496e8e9948bada0";
+String CovalentKey = "ckey_e5e3db3513e0484fb7817f0249e";
+String EtherscanKey = "3ZZZIHZ1RG8JX773Y6FSAWTC6W6XICQQC4";
